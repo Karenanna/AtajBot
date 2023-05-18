8e18fafea3881ba05c8d2ae686c7dd16dac116b7
karen gevorgyan <karen-gev.22@mail.ru>
2023-05-18 15:39:55 +0300

    First Commit

---
+++ src/main/java/pro/sky/atajbot/AtajBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.atajbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtajBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtajBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/atajbot/AtajBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.atajbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtajBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
